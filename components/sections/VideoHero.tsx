14dd1787f5c706f0f414cdee1d9087865ab99cac
Wyatt Leroux <wleroux@axiompartners.ca>
2025-08-26 10:05:48 -0400

    feat: Move Get Started button much further down with mt-[850px]

--- components/sections/VideoHero.tsx
+++ components/sections/VideoHero.tsx
@@ -26,7 +26,7 @@ const VideoHero = () => {
       </div>
 
       {/* Content - Just the Button */}
-<div className="relative z-10 text-center max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 mt-[1000px]">          initial={{ opacity: 0, y: 30 }}
+<div className="relative z-10 text-center max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 mt-[850px]">          initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.5 }}
         >
