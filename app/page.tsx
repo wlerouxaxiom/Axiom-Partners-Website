c7fa4b599c163a7dd6330b3a3bea1e49ec63265f
wlerouxaxiom <wleroux@axiompartners.ca>
2026-01-14 14:32:29 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 import VideoHero from '@/components/sections/VideoHero';
-import ValueProposition from '@/components/sections/ValueProposition';
+import WhatIsAxiom from '@/components/sections/ValueProposition';
 import ServicesOverview from '@/components/sections/ServicesOverview';
-import IndustriesOverview from '@/components/sections/IndustriesOverview';
-import ProBonoSection from '@/components/sections/ProBonoSection';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <VideoHero />
-      <ValueProposition />
+      <WhatIsAxiom />
       <ServicesOverview />
-      <IndustriesOverview />
-      <ProBonoSection />
     </div>
   );
 }
