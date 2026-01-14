9bc5137d5891243fd9d797fe441c563ca08a22cc
wlerouxaxiom <wleroux@axiompartners.ca>
2026-01-14 14:36:14 -0500

    Remove ServicesOverview component from Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import VideoHero from '@/components/sections/VideoHero';
 import ValueProposition from '@/components/sections/ValueProposition';
-import ServicesOverview from '@/components/sections/ServicesOverview';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <VideoHero />
       <ValueProposition />
-      <ServicesOverview />
     </div>
   );
 }
