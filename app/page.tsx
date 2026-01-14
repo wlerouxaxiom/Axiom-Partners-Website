c6a60632e4d2a40f72a7b33ddbb0df1ba59459a3
wlerouxaxiom <wleroux@axiompartners.ca>
2026-01-14 14:33:55 -0500

    Replace WhatIsAxiom with ValueProposition component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import VideoHero from '@/components/sections/VideoHero';
-import WhatIsAxiom from '@/components/sections/ValueProposition';
+import ValueProposition from '@/components/sections/ValueProposition';
 import ServicesOverview from '@/components/sections/ServicesOverview';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <VideoHero />
-      <WhatIsAxiom />
+      <ValueProposition />
       <ServicesOverview />
     </div>
   );
